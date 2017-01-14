5ba7b1f2989ae191fae72f68dc3b8d5ff59d5aae
Ivan Bausov <ivan.bausov@gmail.com>
2017-01-14 20:20:28 +0300

    simple Deferred realization

---
+++ lib/helpers/Deferred.ts
@@ -0,0 +1,27 @@
+/**
+ * @author Ivan Bausov
+ * Date: 14/01/17
+ * Time: 20:09
+ */
+
+export default class Deferred<T> {
+    constructor() {
+        this.promise = new Promise<T>((resolve:(param:T)=>void, reject:(error:any)=>void) => {
+            this.resolve_promise = (param:T) => resolve(param);
+            this.reject_promise = (error:any) => reject(error);
+        });
+    }
+
+    public resolve():Promise<T> {
+        return this.promise;
+    }
+
+    public reject():Promise<T> {
+        return this.promise;
+    }
+
+    private resolve_promise:(param:T) => void;
+    private reject_promise:(error:T) => void;
+
+    private promise:Promise<T>;
+}
